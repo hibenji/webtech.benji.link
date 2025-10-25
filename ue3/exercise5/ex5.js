2127dab5c1d4e6d3be9bd6129529a7702f65cee2
hibenji <benjbordne@outlook.com>
2025-10-25 17:29:56 +0000

    finished ex5

---
+++ ue3/exercise5/ex5.js
@@ -0,0 +1,20 @@
+document.querySelector('#button').addEventListener('click',function (event){
+
+    // isnt actually needed
+    if (event.currentTarget != event.target) return;
+
+    console.log("inner:");
+    console.log("target: "+event.target);
+    console.log("currentTarget: "+event.currentTarget);
+    console.log("thist: "+this);
+});
+
+document.querySelector('#outerArea').addEventListener('click',function (event){
+
+    if (event.currentTarget != event.target) return;
+
+    console.log("outer:");
+    console.log("target: " + event.target);
+    console.log("currentTarget: " + event.currentTarget);
+    console.log("thist: " + this);
+});
