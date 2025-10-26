69ee03380740c63fe503e9a95d318571ef3e438a
hibenji <benjbordne@outlook.com>
2025-10-26 17:27:54 +0000

    done last

---
+++ ue3/exercise6/gallery.js
@@ -0,0 +1,43 @@
+function showImg(e) {
+   document.querySelector("#displayDIV .bigIMG").src=e.target.dataset.large;
+   document.querySelector("#displayDIV .desc").innerHTML=e.target.alt;
+   document.getElementById("displayDIV").style.display="block";
+}
+
+function hideImg(e) {
+   if (e.currentTarget != e.target) return;
+   e.currentTarget.style.display="none";
+}
+
+let imagesLarge = ["hk.JPG", "street.JPG", "taichung.JPG"];
+
+function nextImg() {
+   let currentSrc = document.querySelector("#displayDIV .bigIMG").getAttribute("src")
+   nextIndex = (imagesLarge.indexOf(currentSrc) + 1) % 3
+   console.log(nextIndex)
+   document.querySelector("#displayDIV .bigIMG").src = imagesLarge[nextIndex]
+}
+
+function backImg() {
+   let currentSrc = document.querySelector("#displayDIV .bigIMG").getAttribute("src")
+   nextIndex = (imagesLarge.indexOf(currentSrc) - 1) % 3
+   document.querySelector("#displayDIV .bigIMG").src = imagesLarge[nextIndex]
+}
+
+let images = document.querySelectorAll('.gallery img');
+for (let i=0; i< images.length; i++) {
+   images[i].addEventListener("click", showImg);
+}
+document.getElementById("displayDIV").addEventListener("click", hideImg);
+
+
+buttonBack = document.createElement("button");
+buttonBack.innerHTML = "Back";
+buttonNext = document.createElement("button");
+buttonNext.innerHTML = "Next";
+
+document.getElementById("displayDIV").appendChild(buttonBack);
+document.getElementById("displayDIV").appendChild(buttonNext);
+
+buttonBack.addEventListener("click", backImg);
+buttonNext.addEventListener("click", nextImg);
